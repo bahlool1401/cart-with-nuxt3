d850cc7927be30c0e3c5ebc567ec3f0b56ef6bb2
bahlool1401 <bahlool.ramesh@gmail.com>
2024-08-14 08:13:30 +0330

    edit in iphone section

--- pages/iphone/index.vue
+++ pages/iphone/index.vue
@@ -1,32 +1,13 @@
 <template>
     <div class=" grid grid-cols-4 gap-4 mt-10">
-        <nuxt-link to="/iphone/iphone-12" class="shadow-lg text-center border rounded-lg">
-            <h1>Iphone 12</h1>
+        <nuxt-link v-for="iphone in iphones" :to="`/iphone/${iphone}`" class="shadow-lg text-center border rounded-lg">
+            <h1>{{iphone}}</h1>
             <div class="flex justify-center">
-                <img width="200" src="/images/iphone-12.webp" alt="">
+                <img width="200" :src="`/images/${iphone}.webp`" alt="">
             </div>
         </nuxt-link>
 
-        <nuxt-link to="/iphone/iphone-12-pro" class="shadow-lg text-center border rounded-lg">
-            <h1>Iphone 12 Pro</h1>
-            <div class="flex justify-center">
-                <img width="200" src="/images/iphone-12-pro.webp" alt="">
-            </div>
-        </nuxt-link>
-
-        <nuxt-link to="/iphone/iphone-13" class="shadow-lg text-center border rounded-lg">
-            <h1>Iphone 13</h1>
-            <div class="flex justify-center">
-                <img width="200" src="/images/iphone-13.webp" alt="">
-            </div>
-        </nuxt-link>
-        
-        <nuxt-link to="/iphone/iphone-13-pro" class="shadow-lg text-center border rounded-lg">
-            <h1>Iphone 13 Pro</h1>
-            <div class="flex justify-center">
-                <img width="200" src="/images/iphone-13-pro.webp" alt="">
-            </div>
-        </nuxt-link>
+       
     </div>
 </template>
 
@@ -34,4 +15,7 @@
 useHead({
     title: 'Nuxt3 - Iphones'
 })
+const iphones =['iphone-12','iphone-12-pro','iphone-13','iphone-13-pro']
+const {data} = useFetch("server/api/iphones")
+console.log("🚀 ~ data:", data.value)
 </script>
